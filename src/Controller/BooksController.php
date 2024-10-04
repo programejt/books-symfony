c55031e27a78e7e8b98474ce1dec2e1f3ba8c56e
Viperproo <you@example.com>
2024-10-04 12:16:28 +0200

    Added form errors with visual styles, improved file upload and delete - part 1

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -20,132 +20,167 @@ use App\EventListener\BookAddEventListener;
 #[Route('/books')]
 final class BooksController extends AbstractController
 {
-    #[Route(name: 'app_books_index', methods: ['GET'])]
-    public function index(Request $request, BooksRepository $booksRepository): Response
-    {
-      $currentPage = (int) ($request->get('page') ?? 1);
+  #[Route(name: 'app_books_index', methods: ['GET'])]
+  public function index(
+    Request $request,
+    BooksRepository $booksRepository
+  ): Response
+  {
+    $currentPage = (int) ($request->get('page') ?? 1);
+    $searchValue = $request->get('book-title-or-author');
 
-      $searchValue = $request->get('book-title-or-author');
+    if ($currentPage < 1) {
+      $currentPage = 1;
+    }
 
-      if ($currentPage < 1) {
-        $currentPage = 1;
+    $limit = 12;
+
+    $books = $booksRepository->findPaginated($searchValue, $currentPage, $limit);
+
+    return $this->render('books/index.html.twig', [
+      'searchValue' => $searchValue,
+      'currentPage' => $currentPage,
+      'pagesCount' => ceil($books->count() / $limit),
+      'books' => $books,
+    ]);
+  }
+
+  #[Route('/{id}', name: 'app_books_show', methods: ['GET'])]
+  public function show(Book $book): Response
+  {
+    return $this->render('books/show.html.twig', [
+        'book' => $book,
+    ]);
+  }
+
+  #[Route('/new', name: 'app_books_new', methods: ['GET', 'POST'])]
+  public function new(
+    Request $request,
+    EntityManagerInterface $entityManager,
+    BookAddEventListener $listener
+  ): Response
+  {
+    $book = new Book();
+    $form = $this->createForm(BookType::class, $book);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->persist($book);
+
+      $result = $this->store($form, $book, $entityManager);
+
+      if ($result) {
+        // $listener = $this->container->get(BookAddEventListener::class);
+        $dispatcher = new EventDispatcher();
+
+        $dispatcher->addListener(BookAddEvent::NAME, [$listener, 'onBookAdd']);
+        $dispatcher->dispatch(new BookAddEvent($book), BookAddEvent::NAME);
       }
 
-      $limit = 12;
-
-      $books = $booksRepository->findPaginated($searchValue, $currentPage, $limit);
-
-      return $this->render('books/index.html.twig', [
-          'searchValue' => $searchValue,
-          'currentPage' => $currentPage,
-          'pagesCount' => ceil($books->count() / $limit),
-          'books' => $books,
-      ]);
+      return $this->redirectToRoute('app_books_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/new', name: 'app_books_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, BookAddEventListener $listener): Response
-    {
-        $book = new Book();
-        $form = $this->createForm(BookType::class, $book);
-        $form->handleRequest($request);
+    return $this->render('books/new.html.twig', [
+      'book' => $book,
+      'form' => $form,
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($book);
+  #[Route('/{id}/edit', name: 'app_books_edit', methods: ['GET', 'POST'])]
+  public function edit(
+    Request $request,
+    Book $book,
+    EntityManagerInterface $entityManager
+  ): Response
+  {
+    $bookOriginal = clone $book;
+    $form = $this->createForm(BookType::class, $book);
+    $form->handleRequest($request);
 
-            $result = $this->store($form, $book, $entityManager);
-
-            if ($result) {
-              // $listener = $this->container->get('BookAddEventListener::class');
-              $dispatcher = new EventDispatcher();
-
-              $dispatcher->addListener(BookAddEvent::NAME, [$listener, 'onBookAdd']);
-              $dispatcher->dispatch(new BookAddEvent($book), BookAddEvent::NAME);
-            }
-
-            return $this->redirectToRoute('app_books_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('books/new.html.twig', [
-            'book' => $book,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_books_show', methods: ['GET'])]
-    public function show(Book $book): Response
-    {
-        return $this->render('books/show.html.twig', [
-            'book' => $book,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_books_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Book $book, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(BookType::class, $book);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->store($form, $book, $entityManager);
-
-            return $this->redirectToRoute('app_books_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('books/edit.html.twig', [
-            'book' => $book,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_books_delete', methods: ['POST'])]
-    public function delete(Request $request, Book $book, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($book);
-            $entityManager->flush();
-        }
+    if ($form->isSubmitted() && $form->isValid()) {
+        $this->store($form, $book, $entityManager);
 
         return $this->redirectToRoute('app_books_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    private function store(
-      FormInterface &$form,
-      Book &$book,
-      EntityManagerInterface &$entityManager
-    ): bool {
-      $newPhoto = $form->get('photo')->getData();
-      $deletePhoto = $form->has('delete-photo') && $form->get('delete-photo');
-      $photo = $book->getPhoto();
+    return $this->render('books/edit.html.twig', [
+        'book' => $bookOriginal,
+        'form' => $form,
+    ]);
+  }
 
-      if ($photo && $deletePhoto) {
-
-      } else if ($newPhoto) {
-        $newFilename = 'book'.'-'.uniqid().'.'.$newPhoto->guessExtension();
-        $book->setPhoto($newFilename);
-      }
-
-      try {
+  #[Route('/{id}', name: 'app_books_delete', methods: ['POST'])]
+  public function delete(
+    Request $request,
+    Book $book,
+    EntityManagerInterface $entityManager
+  ): Response
+  {
+    if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->getPayload()->getString('_token'))) {
+        $entityManager->remove($book);
         $entityManager->flush();
-      } catch (ORMException $e) {
+    }
+
+    return $this->redirectToRoute('app_books_index', [], Response::HTTP_SEE_OTHER);
+  }
+
+  private function store(
+    FormInterface &$form,
+    Book &$book,
+    EntityManagerInterface &$entityManager
+  ): bool {
+    $newPhoto = $form->get('photo')->getData();
+    $deletePhoto = $form->has('delete-photo') && $form->get('delete-photo');
+    $photo = $book->getPhoto();
+
+    if ($photo && $deletePhoto) {
+    } else if ($newPhoto) {
+      $fileExtension = strtolower($newPhoto->guessExtension());
+
+      if (!$this->validateBookPhotoExtension($fileExtension)) {
         return false;
       }
 
-      if ($newPhoto) {
-        $bookId = $book->getId();
-        $photoPath = "assets/images/books/$bookId";
+      $newFilename = 'book'.'-'.bin2hex(random_bytes(13)).'.'.$fileExtension;
+      $book->setPhoto($newFilename);
+    }
 
-        if ($photo && file_exists("$photoPath/$photo")) {
-          unlink("$photoPath/$photo");
-        }
+    try {
+      $entityManager->flush();
+    } catch (ORMException $e) {
+      return false;
+    }
 
-        try {
-          $newPhoto->move($photoPath, $newFilename);
-        } catch (FileException $e) {
-          return false;
-        }
+    $photoPath = $book->getSystemPhotosPath();
+    $photo = str_replace(['/', '..', '\\'], '', $photo);
+    $photoFullPath = str_replace('..', '', $photoPath)."/$photo";
+
+    if (!($photo && $deletePhoto) && $newPhoto) {
+      if ($photo) {
+        $this->deletePhoto($photoFullPath);
       }
 
-      return true;
+      try {
+        $newPhoto->move($photoPath, $newFilename);
+      } catch (FileException $e) {
+        return false;
+      }
     }
+
+    return true;
+  }
+
+  private function deletePhoto(string $photoFullPath): bool {
+    if (file_exists($photoFullPath)) {
+      return unlink($photoFullPath);
+    }
+    return false;
+  }
+
+  private function validateBookPhotoExtension(string $fileExtension): bool {
+    return match ($fileExtension) {
+      'jpg', 'jpeg', 'png', 'webp', 'avif', 'heif' => true,
+      default => false
+    };
+  }
 }
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -15,50 +15,49 @@ use Symfony\Component\Form\FormEvents;
 
 class BookType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('title')
-            ->add('author')
-            ->add('description', TextareaType::class)
-            ->add('year')
-            ->add('isbn')
-            ->add('photo', FileType::class, [
-              'data_class' => null,
-              'required' => false,
-              'constraints' => [
-                new File([
-                    'maxSize' => '25m',
-                    'mimeTypes' => [
-                        'image/jpg',
-                        'image/jpeg',
-                        'image/png',
-                        'image/webp'
-                    ],
-                    'mimeTypesMessage' => 'Please upload a valid image',
-                ])
-              ],
-            ])
-        ;
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('title')
+      ->add('author')
+      ->add('description', TextareaType::class)
+      ->add('year')
+      ->add('isbn')
+      ->add('photo', FileType::class, [
+        'data_class' => null,
+        'required' => false,
+        'constraints' => [
+          new File([
+            'maxSize' => '25m',
+            'mimeTypes' => [
+                'image/jpg',
+                'image/jpeg',
+                'image/png',
+                'image/webp'
+            ],
+            'mimeTypesMessage' => 'Please upload a valid image',
+          ])
+        ],
+      ]);
 
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
-          $book = $event->getData();
-          $form = $event->getForm();
+    $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+      $book = $event->getData();
+      $form = $event->getForm();
 
-          if ($book && null !== $book->getPhoto()) {
-              $form->add('deletePhoto', CheckboxType::class, [
-                'mapped' => false,
-                'required' => false,
-                'label' => 'Delete photo'
-              ]);
-          }
-      });
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Book::class,
+      if ($book && null != $book->getPhoto()) {
+        $form->add('deletePhoto', CheckboxType::class, [
+          'mapped' => false,
+          'required' => false,
+          'label' => 'Delete photo'
         ]);
-    }
+      }
+    });
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+        'data_class' => Book::class,
+    ]);
+  }
 }
