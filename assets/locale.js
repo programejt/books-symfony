b5e837004ab1fc35514b2fc3f5096e9c20d8ab9f
Programejt <you@example.com>
2025-08-29 13:34:50 +0200

    Change let variables to const in locale.js and theme.js

--- assets/locale.js
+++ assets/locale.js
@@ -2,15 +2,15 @@
   const html = document.documentElement;
 
   document.addEventListener('change', function (ev) {
-    let localeInput = ev.target;
+    const localeInput = ev.target;
 
     if (localeInput.matches('input.locale-input')) {
-      let locale = localeInput.value;
+      const locale = localeInput.value;
+      const d = new Date();
+
+      d.setTime(d.getTime() + (150 * 24 * 60 * 60 * 1000));
 
       html.lang = locale;
-
-      const d = new Date();
-      d.setTime(d.getTime() + (150 * 24 * 60 * 60 * 1000));
 
       document.cookie = 'locale=' + locale + '; expires=' + d.toUTCString() + ';path=/;sameSite=lax;';
 
--- assets/theme.js
+++ assets/theme.js
@@ -2,15 +2,15 @@
   const html = document.documentElement;
 
   document.addEventListener('change', function (ev) {
-    let themeInput = ev.target;
+    const themeInput = ev.target;
 
     if (themeInput.matches('input.toggle-theme')) {
-      let theme = themeInput.value;
+      const theme = themeInput.value;
+      const d = new Date();
+
+      d.setTime(d.getTime() + (150 * 24 * 60 * 60 * 1000));
 
       html.setAttribute('data-bs-theme', theme);
-
-      const d = new Date();
-      d.setTime(d.getTime() + (150 * 24 * 60 * 60 * 1000));
 
       document.cookie = 'theme=' + theme + '; expires=' + d.toUTCString() + ';path=/;sameSite=lax;';
     }
